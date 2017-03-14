24d02bc3ff1276c97b0e4abef281d31cd828085d
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-03-14 14:33:07 +0100

    foo

--- profile/manifests/openstack/designate.pp
+++ profile/manifests/openstack/designate.pp
@@ -1,21 +1,24 @@
 class profile::openstack::designate (
   $manage_designate = false,
-  $manage_designate_bind = false,
   $my_zones = {},
   $my_nameservers = {},
   $my_pools = {},
   $my_targets = {},
-){
+  $dns_master = {},
+  $dns_slave01 = {},
+  $dns_slave02 = {},
+  $mdns_server = {},
+)
+{
   include ::designate
   if $manage_designate {
     include ::designate::db
     include ::designate::api
     include ::designate::central
     include ::designate::pool_manager
-#    include ::designate::pool_manager_cache::memcache
     include ::designate::mdns
     include ::designate::sink
-#    include ::designate::backend::bind9
+
     if $my_nameservers {
        create_resources('designate::pool_nameserver', $my_nameservers)
     }
@@ -38,44 +41,5 @@ class profile::openstack::designate (
       refreshonly => true,
     }
 
-  }
-  if $manage_designate_bind {
-    package { 'bind':
-      ensure => installed,
-    } ->
-    exec { '/usr/sbin/rndc-confgen -a':
-      creates => '/etc/rndc.key',
-    } ->
-    file { '/etc/rndc.key':
-      mode         => '0600',
-      owner        => 'named',
-      group        => 'named',
-    } ->
-    file { '/etc/rndc.conf':
-      content      => template("${module_name}/openstack/designate/rndc.conf.erb"),
-      mode         => '0640',
-      owner        => 'named',
-      group        => 'named',
-    } ->
-    file { '/etc/named.conf':
-      content      => template("${module_name}/openstack/designate/named.conf.erb"),
-      mode         => '0640',
-      owner        => 'root',
-      group        => 'named',
-      validate_cmd => '/usr/sbin/named-checkconf %',
-    } ->
-    file { '/var/named':
-      ensure       => directory,
-      mode         => '0770',
-      owner        => 'root',
-      group        => 'named',
-    } ->
-    exec { '/usr/sbin/setsebool -P named_write_master_zones on':
-      unless => "/usr/sbin/getsebool named_write_master_zones | /usr/bin/egrep -q 'on$'",
-    } ->
-    service { 'named':
-      ensure => running,
-      enable => true,
-    }
   }
 }
