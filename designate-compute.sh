23939b31459573ad26d0b51d22a5859955da97b2
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-03-28 12:38:59 +0200

    nova config for designate

--- designate-compute.sh
+++ designate-compute.sh
@@ -2,22 +2,6 @@
 #
 # run as root!
 
-# Configure [DEFAULT] in nova.conf
-openstack-config --set /etc/nova/nova.conf DEFAULT notify_on_state_change vm_and_task_state
-openstack-config --set /etc/nova/nova.conf DEFAULT instance_usage_audit_period hour
-openstack-config --set /etc/nova/nova.conf DEFAULT instance_usage_audit true
-
-# Configure [oslo_messaging_notifications] in nova.conf
-openstack-config --set /etc/nova/nova.conf oslo_messaging_notifications driver messagingv2
-openstack-config --set /etc/nova/nova.conf oslo_messaging_notifications topics notifications
-
-# Restart services
-openstack-service restart nova
-systemctl restart openstack-nova-compute
-systemctl restart openstack-nova-metadata-api
-
-
-
 # qemu stuff
 cat <<EOF >/etc/yum.repos.d/trondham.repo
 [trondham]
