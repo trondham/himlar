61b0bdc9a7472d72fd49fd5afa64c7138621280e
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-04-03 14:32:59 +0200

    openrc

--- designate-deploy.sh
+++ designate-deploy.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # run as root!
 
 ZONE_NAME='vagrant.uh-iaas.no'
 
-. /root/openrc
+#
+# Managed by puppet in profile::openstack::openrc
+#
+export OS_USERNAME=admin
+export OS_PROJECT_NAME=openstack
+export OS_PASSWORD=admin_pass
+export PS1='[\u@\h \W(keystone_vagrant_admin)]$ '
+
+#. /root/openrc
 
 # update pool config (FIXME: should be done by puppet)
 designate-manage pool update --file /etc/designate/zone_config.yaml
 
 # Tenant ID to own all managed resources - like auto-created records etc.
-OPENSTACK_TENANT_ID=$(openstack project show services -c id -f value)
+OPENSTACK_TENANT_ID=$(openstack project show openstack -c id -f value)
 openstack-config --set /etc/designate/designate.conf service:central managed_resource_tenant_id $OPENSTACK_TENANT_ID
 
 # Restart services
