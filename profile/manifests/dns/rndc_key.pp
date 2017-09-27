1544e1b8f4ca8f4d1a4e20bb1858e1dcac12510c
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-09-27 17:29:50 +0200

    foo

--- profile/manifests/dns/rndc_key.pp
+++ profile/manifests/dns/rndc_key.pp
@@ -1,14 +1,15 @@
-class profile::dns::rndc_key ()
+class profile::dns::rndc_key (
+  $create_admin_key  = false,
+  $create_mdns_key   = false,
+  $rndc_secret_admin = $profile::dns::rndc_secret_admin,
+  $rndc_secret_mdns  = $profile::dns::rndc_secret_mdns
+)
 {
-#  $rndc = hiera_hash('profile::dns::rndc_keys', {})
 
-#  key { $rndc[0]:
-#    name   => $rndc[0],
-#    secret => $rndc[1],
-#  }
-
-  hiera_hash('profile::dns::rndc_keys', {}).each |$rndc_key_name, $rndc_key_secret| {
-    file { "/etc/rndc-${rndc_key_name}.key":
+  if ($create_admin_key == true) {
+    $name = 'admin'
+    $secret = $rndc_secret_admin
+    file { "/etc/rndc-admin.key":
       content      => template("${module_name}/dns/bind/rndc.key.erb"),
       notify       => Service['named'],
       mode         => '0640',
@@ -18,4 +19,16 @@ class profile::dns::rndc_key ()
     }
   }
 
+  if ($create_mdns_key == true) {
+    $name = 'mdns'
+    $secret = $rndc_secret_mdns
+    file { "/etc/rndc_secret_mdns.key":
+      content      => template("${module_name}/dns/bind/rndc.key.erb"),
+      notify       => Service['named'],
+      mode         => '0640',
+      owner        => 'named',
+      group        => 'named',
+      require      => Package['bind'],
+    }
+  }
 }
