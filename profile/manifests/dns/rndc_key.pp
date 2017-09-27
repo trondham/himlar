3e3081bd52818adcd9ebef7cf7ded5b07eefb1d2
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-09-27 16:58:49 +0200

    foo

--- profile/manifests/dns/rndc_key.pp
+++ profile/manifests/dns/rndc_key.pp
@@ -1,14 +1,22 @@
 class profile::dns::rndc_key (
-  $rndc_key_name   = $profile::dns::rndc_key_name,
-  $rndc_key_secret = $profile::dns::rndc_key_secret
 )
 {
-  file { "/etc/rndc-${rndc_key_name}.key":
-    content      => template("${module_name}/dns/bind/rndc.key.erb"),
-    notify       => Service['named'],
-    mode         => '0640',
-    owner        => 'named',
-    group        => 'named',
-    require      => Package['bind'],
+#  $rndc = hiera_hash('profile::dns::rndc_keys', {})
+
+#  key { $rndc[0]:
+#    name   => $rndc[0],
+#    secret => $rndc[1],
+#  }
+
+  hiera_hash('profile::dns::rndc_keys').each |String $rndc_key_name, String $rndc_key_secret| {
+    file { "/etc/rndc-${rndc_key_name}.key":
+      content      => template("${module_name}/dns/bind/rndc.key.erb"),
+      notify       => Service['named'],
+      mode         => '0640',
+      owner        => 'named',
+      group        => 'named',
+      require      => Package['bind'],
+    }
   }
+
 }
