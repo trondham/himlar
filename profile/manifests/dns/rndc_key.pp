d1c894cf10e15905eace466bc1a65b110f3f7fc3
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-09-27 17:59:33 +0200

    foo

--- profile/manifests/dns/rndc_key.pp
+++ profile/manifests/dns/rndc_key.pp
@@ -18,7 +18,7 @@ class profile::dns::rndc_key (
   }
 
   if $create_admin_key {
-    create_keyfile('admin', $rndc_secret_admin)
+    profile::dns::rndc_key::create_keyfile('admin', $rndc_secret_admin)
 #    $this_rndc_name = 'admin'
 #    $this_rndc_secret = $rndc_secret_admin
 #    file { "/etc/rndc-admin.key":
@@ -32,7 +32,7 @@ class profile::dns::rndc_key (
   }
 
   if $create_mdns_key {
-    create_keyfile('mdns', $rndc_secret_mdns)
+    profile::dns::rndc_key::create_keyfile('mdns', $rndc_secret_mdns)
 #    $this_rndc_name = 'mdns'
 #    $this_rndc_secret = $rndc_secret_mdns
 #    file { "/etc/rndc-mdns.key":
