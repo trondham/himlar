ef091fc77ba713cbc725ca4e9cf88bdc83e209bf
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-09-27 18:19:59 +0200

    arrrgh!

--- profile/manifests/dns/rndc_key.pp
+++ profile/manifests/dns/rndc_key.pp
@@ -6,9 +6,9 @@ class profile::dns::rndc_key (
 )
 {
 
-  define profile::dns::rndc_key::create_keyfile ($name = {}, $secret = {}) {
-    file { "/etc/rndc-${name}.key":
-      content      => template("${module_name}/dns/bind/rndc.key.erb"),
+  if $create_admin_key {
+    file { "/etc/rndc-admin.key":
+      content      => template("${module_name}/dns/bind/rndc-admin.key.erb"),
       notify       => Service['named'],
       mode         => '0640',
       owner        => 'named',
@@ -17,32 +17,14 @@ class profile::dns::rndc_key (
     }
   }
 
-  if $create_admin_key {
-    create_resources('profile::dns::rndc_key::create_keyfile', {$name = 'admin', $secret = $rndc_secret_admin})
-#    $this_rndc_name = 'admin'
-#    $this_rndc_secret = $rndc_secret_admin
-#    file { "/etc/rndc-admin.key":
-#      content      => template("${module_name}/dns/bind/rndc.key.erb"),
-#      notify       => Service['named'],
-#      mode         => '0640',
-#      owner        => 'named',
-#      group        => 'named',
-#      require      => Package['bind'],
-#    }
-  }
-
   if $create_mdns_key {
-    create_resources('profile::dns::rndc_key::create_keyfile', {$name = 'mdns', $secret = $rndc_secret_mdns})
-#    profile::dns::rndc_key::create_keyfile('mdns', $rndc_secret_mdns)
-#    $this_rndc_name = 'mdns'
-#    $this_rndc_secret = $rndc_secret_mdns
-#    file { "/etc/rndc-mdns.key":
-#      content      => template("${module_name}/dns/bind/rndc.key.erb"),
-#      notify       => Service['named'],
-#      mode         => '0640',
-#      owner        => 'named',
-#      group        => 'named',
-#      require      => Package['bind'],
-#    }
+    file { "/etc/rndc-mdns.key":
+      content      => template("${module_name}/dns/bind/rndc-mdns.key.erb"),
+      notify       => Service['named'],
+      mode         => '0640',
+      owner        => 'named',
+      group        => 'named',
+      require      => Package['bind'],
+    }
   }
 }
