c6608b3a80bdab2562b028c2a4ee8cc0d4c82cfd
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-10-03 15:55:59 +0200

    define

--- profile/manifests/dns/ns.pp
+++ profile/manifests/dns/ns.pp
@@ -99,10 +99,8 @@ class profile::dns::ns (
     }
   }
 
-  define reverse_zone(
-    $cidr,
-    $origin,
-    $filename) {
+  define reverse_zone($cidr, $origin, $filename) {
+    $internal_zone = $profile::dns::ns::internal_zone
     file { "/var/named/$filename":
       content      => template("${module_name}/dns/bind/reverse_zone.erb"),
       notify       => Service['named'],
