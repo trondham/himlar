5fbc258215bbd05460291e7577eb468e244a034f
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-11-02 15:55:52 +0100

    test

--- profile/manifests/dns/forward_zone.pp
+++ profile/manifests/dns/forward_zone.pp
@@ -2,8 +2,7 @@ define profile::dns::forward_zone($zone, $filename) {
   $internal_zone = $::profile::dns::ns::internal_zone
 
   # Our name servers
-  #$name_servers = hiera_array('profile::dns::ns::name_servers', {})
-  $name_servers = $::profile::dns::ns::name_servers
+  $name_servers = hiera_array('profile::dns::ns::name_servers', {})
 
   file { "/var/named/${filename}":
     content      => template("${module_name}/dns/bind/forward_zone.erb"),
