e27d04c94057f1fce4af327aabf946c59fb2cd72
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-02-22 10:37:51 +0100

    test

--- profile/manifests/dns/forward_zone.pp
+++ profile/manifests/dns/forward_zone.pp
@@ -3,8 +3,8 @@ define profile::dns::forward_zone($zone, $filename) {
   $mail    = $::profile::dns::ns::mail
 
   # Our name servers
-  $master = lookup('profile::dns::ns::name_servers', Hash[String], 'master', 'unique', [])
-  $slaves = lookup('profile::dns::ns::name_servers', Hash[String], 'slaves', 'unique', [])
+  $ns_master = $::profile::dns::ns::ns_master
+  $ns_slaves = lookup('profile::dns::ns::ns_slaves', Array, 'seep', [])
 
   file { "/var/named/${filename}":
     content => template("${module_name}/dns/bind/forward_zone.erb"),
