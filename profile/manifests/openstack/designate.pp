b7114826dac14807657298527d47dd798bd2c558
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-07-05 14:40:00 +0200

    foo

--- profile/manifests/openstack/designate.pp
+++ profile/manifests/openstack/designate.pp
@@ -25,16 +25,6 @@ class profile::openstack::designate (
     type => 'targeted',
   }
 
-#  if $my_nameservers {
-#    create_resources('designate::pool_nameserver', $my_nameservers)
-#  }
-#  if $my_pools {
-#    create_resources('designate::pool', $my_pools)
-#  }
-#  if $my_targets {
-#    create_resources('designate::pool_target', $my_targets)
-#  }
-
   file { '/etc/designate/pools.yaml':
     content      => template("${module_name}/openstack/designate/pools.yaml.erb"),
     mode         => '0644',
