b22fd0e6f623c1fff4393604363376a369418292
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-10-09 13:35:04 +0200

    foo

--- profile/manifests/base/dell.pp
+++ profile/manifests/base/dell.pp
@@ -14,13 +14,6 @@ class profile::base::dell
     create_resources('yumrepo', $repo_hash) ->
     create_resources('profile::base::package', $packages)
 
-    # SNMP daemon
-    service { "snmpd":
-      ensure  => running,
-      enable  => true,
-      require => Package['net-snmp']
-    }
-
     # OMSA daemons
     service { "instsvcdrv":
       ensure => running,
@@ -44,6 +37,13 @@ class profile::base::dell
       command => "/etc/init.d/dataeng enablesnmp",
       unless  => "/bin/grep -q ^smuxpeer /etc/snmp/snmpd.conf",
       notify  => Service['snmpd'],
+    }
+
+    # SNMP daemon
+    service { "snmpd":
+      ensure  => running,
+      enable  => true,
+      require => Package['net-snmp']
     }
 
   }
