58c4409046fb3eff06115c0283cac5c4dc6736d7
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-11-15 13:05:05 +0100

    foo

--- profile/manifests/openstack/designate.pp
+++ profile/manifests/openstack/designate.pp
@@ -3,7 +3,7 @@ class profile::openstack::designate (
   $my_nameservers = {},
   $my_pools = {},
   $my_targets = {},
-  $mdns_transport_addr = [ '127.0.0.0', '127.0.0.2' ],
+  $mdns_transport_addr = lookup('profile::dns::ns::mdns_transport_addr', Array, 'deep', []),
   $manage_firewall = false,
   $bind_servers = lookup('profile::openstack::designate::bind_servers', Hash, 'first', {})
 )
