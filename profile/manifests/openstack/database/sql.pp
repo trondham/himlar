6b09265711fd3b2e2f693cbac54744134b88ce64
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2019-10-20 13:31:46 +0200

    foo

--- profile/manifests/openstack/database/sql.pp
+++ profile/manifests/openstack/database/sql.pp
@@ -7,6 +7,7 @@ class profile::openstack::database::sql (
   $trove_enabled    = false,
   $cinder_enabled   = false,
   $designate_enabled = false,
+  $congress_enabled = false,
   $gnocchi_enabled  = false,
   $database         = 'mariadb',
   $extra_databases  = {},
