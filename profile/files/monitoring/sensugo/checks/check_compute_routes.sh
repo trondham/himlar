44284a9bc3c296cbeba0259603d74e930b6aa32d
Raymond Kristiansen <raymond.kristiansen@uib.no>
2026-04-15 11:03:39 +0200

    feat(compute): route check can not use running vms

--- profile/files/monitoring/sensugo/checks/check_compute_routes.sh
+++ profile/files/monitoring/sensugo/checks/check_compute_routes.sh
@@ -1,52 +1,35 @@
 #!/bin/bash
 
+# ipv6 exported bird routes
 rr1_ipv6=$(birdc show route export rr1_ipv6 | grep kernel | wc -l)
 rr2_ipv6=$(birdc show route export rr2_ipv6 | grep kernel | wc -l)
 
+# ipv4 exported bird routes
 rr1_ipv4=$(birdc show route export rr1_ipv4 | grep kernel | wc -l)
 rr2_ipv4=$(birdc show route export rr2_ipv4 | grep kernel | wc -l)
 
-running_vms=$(virsh list --state-running | grep -c 'running')
-
+# kernel routes added by calico
 ipv6_routes=$(ip -6 r | grep tap | grep -v 'fe80::/64' | wc -l)
 ipv4_routes=$(ip -br r | grep tap | wc -l)
 
-if [ "${rr1_ipv4}" -eq "${running_vms}" ] || [ ${rr2_ipv4} -eq ${running_vms} ]; then
-    echo "IPv4 bird prefix match running vms"
+if [ "${rr1_ipv4}" -eq "${ipv4_routes}" ] || [ ${rr2_ipv4} -eq ${ipv4_routes} ]; then
+    echo "IPv4 bird prefix match kernel routes"
 else
-    echo "IPv4 bird prefix do not match running vms"
-    echo "Running VMs=${running_vms}"
+    echo "IPv4 bird prefix do not match kernel routes"
+    echo "Kernel routes=${ipv4_routes}"
     echo "IPv4 rr1 prefix=${rr1_ipv4}"
     echo "IPv4 rr2 prefix=${rr2_ipv4}"
-    exit 1
+#    exit 1
 fi
 
-if [ "${ipv4_routes}" -eq "${running_vms}" ]; then
-    echo "IPv4 routes match running vms"
+if [ "${rr1_ipv6}" -eq "${ipv6_routes}" ] || [ ${rr2_ipv6} -eq ${ipv6_routes} ]; then
+    echo "IPv6 bird prefix match kernel routes"
 else
-    echo "IPv4 routes do not match running vms"
-    echo "Running VMs=${running_vms}"
-    echo "IPv4 routes=${ipv4_routes}"
-    exit 1
-fi
-
-if [ "${rr1_ipv6}" -eq "${running_vms}" ] || [ ${rr2_ipv6} -eq ${running_vms} ]; then
-    echo "IPv6 bird prefix match running vms"
-else
-    echo "IPv6 bird prefix do not match running vms"
-    echo "Running VMs=${running_vms}"
+    echo "IPv6 bird prefix do not match kernel routes"
+    echo "Kernel routes=${ipv6_routes}"
     echo "IPv6 rr1 prefix=${rr1_ipv6}"
     echo "IPv6 rr2 prefix=${rr2_ipv6}"
-    exit 1
-fi
-
-if [ "${ipv6_routes}" -eq "${running_vms}" ]; then
-    echo "IPv6 routes match running vms"
-else
-    echo "IPv6 routes do not match running vms"
-    echo "Running VMs=${running_vms}"
-    echo "IPv6 routes=${ipv6_routes}"
-    exit 1
+#    exit 1
 fi
 
 exit 0
